57770039a60d089f1caaf80cb3282077b578cf50
dz <dz.int.dev@pm.me>
2018-10-07 21:59:18 +0200

    UI Integration - adding RESTful controller for API calls - adding NewProcessController for convenience - modifying Spring boot JSON output with indentation

---
+++ src/main/java/com/example/alva/ui/NewProcessController.java
@@ -0,0 +1,25 @@
+package com.example.alva.ui;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.alva.storage.VisitorProcess;
+
+@Controller
+public class NewProcessController {
+
+    private final RestAPIController apiController;
+
+    @Autowired
+    public NewProcessController(final RestAPIController apiController) {this.apiController = apiController;}
+
+    @GetMapping("/new")
+    public ResponseEntity<VisitorProcess> startNewVisitor(HttpServletRequest request, @RequestParam final String url) {
+        return this.apiController.addNewVisit(request, url);
+    }
+}
